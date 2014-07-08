19945549894341a9694c5ded08f0dda4efc81bc2
maxfriedmann <maximilian.friedmann@gmail.com>
2014-07-08 16:50:49 +0200

    Update Gruntfile.js

--- test/Gruntfile.js
+++ test/Gruntfile.js
@@ -48,8 +48,8 @@ module.exports = function(grunt)
 				"saucelabs_connect" : {
 					"standalone" : false,
 					"launchUrl" : "http://localhost:3000",
-					"selenium_host" : "127.0.0.1",
-					"selenium_port" : 4445,
+					"selenium_host" : "localhost",
+					"selenium_port" : "4445",
 					"username" : "maxfriedmann",
 					"access_key" : "9f608584-3969-4639-b95e-b4f3efbec2d9"
 				}
